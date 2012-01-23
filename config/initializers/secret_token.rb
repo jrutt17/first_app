fca91fa886729299b4ac1c8c79d33df7624003f2
System Administrator <root@ny-switch-07.dana.local>
2012-01-23 11:23:57 -0500

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '80f4ee27914681b48cca8ff82e580200f28177ea03de150a49f254627fd57951b11307639da7e85ef1d43c33d9043198439de81729978df4e45d9a9ab2c5f39e'
